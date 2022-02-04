b3159b9f592a2f67db210ef42b51bbc7a10a944b
Andrew Shukhtin <shukhtin.andrey@mail.ru>
2022-02-04 23:35:45 +0300

    remove unnecessary formatters

--- lua/lsp/null_ls.lua
+++ lua/lsp/null_ls.lua
@@ -8,14 +8,9 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
 	sources = {
 		formatting.prettier,
-		formatting.black,
 		formatting.gofmt,
 		formatting.shfmt,
-		formatting.clang_format,
-		formatting.cmake_format,
-		formatting.dart_format,
 		formatting.stylua,
-		formatting.isort,
 		formatting.codespell.with({ filetypes = { "markdown" } }),
 	},
 	-- on_attach = function(client)
