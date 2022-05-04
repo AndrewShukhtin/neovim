a93571e0655f3089875e9083c985f56ff2f51faa
Andrew Shukhtin <shukhtin.andrey@mail.ru>
2022-05-05 01:05:54 +0300

    changed alpha

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -46,7 +46,7 @@ local headers = {
 }
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = headers.chernaya100
+dashboard.section.header.val = headers.uwu
 dashboard.section.buttons.val = {
 	dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "▶ New file", ":ene <BAR> startinsert <CR>"),
