6ed294df7807e12f5cc39c2342f9396e66b5f04c
Andrew Shukhtin <shukhtin.andrey@mail.ru>
2022-05-04 23:23:50 +0300

    comment remaps for term mode

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -1,5 +1,5 @@
 require("toggleterm").setup({
-	size = 13,
+	size = 18,
 	open_mapping = [[<c-\>]],
 	shade_filetypes = {},
 	shade_terminals = true,
@@ -8,3 +8,15 @@ require("toggleterm").setup({
 	persist_size = true,
 	direction = "horizontal",
 })
+
+-- function _G.set_terminal_keymaps()
+--   local opts = {noremap = true}
+--   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+-- end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
