083709fd725cb07089cf785f5b8ed87bcc9a8cd4
Андрей Шухтин <vendroid@MacBook-Air-Natalia.local>
2022-07-23 19:57:17 +0300

    apply linter suggestion

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -60,7 +60,7 @@ cmp.setup({
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping.complete({}),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
