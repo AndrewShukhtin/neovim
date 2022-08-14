6117699fc8964cdc312579ae363d01a3597a297e
Андрей Шухтин <vendroid@MacBook-Air-Natalia.local>
2022-08-14 17:05:57 +0300

    add safe load for blanklines

--- lua/blankline-config/init.lua
+++ lua/blankline-config/init.lua
@@ -1,4 +1,9 @@
-require("indent_blankline").setup({
+local ok, indent_blankline = pcall(require, "indent_blankline")
+if not ok then
+  return
+end
+
+indent_blankline.setup({
   buftype_exclude = { "terminal", "alpha", "alpha-nvim", "nofile" },
   filetype_exclude = { "lspinfo", "NvimTree", "packer", "alpha", "alpha-nvim", "nofile" },
   show_end_of_line = true,
