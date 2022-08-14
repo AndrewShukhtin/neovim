8d2e175682e27dabebbb2cff7263fb47dbbc6cce
Андрей Шухтин <vendroid@MacBook-Air-Natalia.local>
2022-08-14 17:00:03 +0300

    add safe load for autopairs-config

--- lua/autopairs-config/init.lua
+++ lua/autopairs-config/init.lua
@@ -1,7 +1,20 @@
-require("nvim-autopairs").setup({
+local autopairs_ok, autopairs = pcall(require, "nvim-autopairs")
+if not autopairs_ok then
+  return
+end
+
+autopairs.setup({
   check_ts = true,
 })
 
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
+local cmp_autopairs_ok, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if not cmp_autopairs_ok then
+  return
+end
+
+local cmp_ok, cmp = pcall(require, "cmp")
+if not cmp_ok then
+  return
+end
+
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
